ace7b6ae6f4867aa2f899b21fff851a2cbde3c4a
Stephen Harris <trackmastersteve@users.noreply.github.com>
2026-08-10 22:02:47 -0700

    Change old world flying trainer spell entries
    Updated SQL script to modify trainer spells for old world flying.

--- data/sql/db-world/base/db_world_mod_fly_in_old_world_trainers.sql
+++ data/sql/db-world/base/db_world_mod_fly_in_old_world_trainers.sql
@@ -1,6 +1,6 @@
--- Add Custom Old World Flying to Outland & Northrend Flight Trainers
-DELETE FROM `npc_trainer` WHERE `SpellId` = 200001;
-INSERT INTO `npc_trainer` (`ID`, `SpellId`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`, `ReqSpell`) VALUES
-(31238, 200001, 5000000, 773, 225, 60, 0), -- Hira Snowdawn (Dalaran)
-(16654, 200001, 5000000, 773, 225, 60, 0), -- Hargen Bronzewing (Honor Hold)
-(17502, 200001, 5000000, 773, 225, 60, 0); -- Wind Rider Jahubo (Thrallmar)
+-- Add Custom Old World Flying to Trainers
+DELETE FROM trainer_spell WHERE SpellId = 200001;
+INSERT INTO trainer_spell (TrainerId, SpellId, MoneyCost, ReqSkillLine, ReqSkillRank, ReqAbility1, ReqAbility2, ReqAbility3, ReqLevel, VerifiedBuild) VALUES 
+(31238, 200001, 5000000, 773, 225, 0, 0, 0, 60, 0),
+(16654, 200001, 5000000, 773, 225, 0, 0, 0, 60, 0),
+(17502, 200001, 5000000, 773, 225, 0, 0, 0, 60, 0);
