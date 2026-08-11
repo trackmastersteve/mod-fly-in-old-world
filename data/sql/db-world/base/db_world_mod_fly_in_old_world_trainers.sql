52a0ccc2e085cde8b3082e2ea750d102fc61d9ad
Stephen Harris <trackmastersteve@users.noreply.github.com>
2026-08-11 07:57:59 -0700

    Update trainer spells for old world flying

--- data/sql/db-world/base/db_world_mod_fly_in_old_world_trainers.sql
+++ data/sql/db-world/base/db_world_mod_fly_in_old_world_trainers.sql
@@ -1,6 +1,6 @@
 -- Add Custom Old World Flying to Trainers
 DELETE FROM trainer_spell WHERE SpellId = 200001;
 INSERT INTO trainer_spell (TrainerId, SpellId, MoneyCost, ReqSkillLine, ReqSkillRank, ReqAbility1, ReqAbility2, ReqAbility3, ReqLevel, VerifiedBuild) VALUES 
-(31238, 200001, 5000000, 773, 225, 0, 0, 0, 60, 0),
-(16654, 200001, 5000000, 773, 225, 0, 0, 0, 60, 0),
-(17502, 200001, 5000000, 773, 225, 0, 0, 0, 60, 0);
+(31238, 200001, 5000000, 773, 150, 0, 0, 0, 60, 0),
+(16654, 200001, 5000000, 773, 150, 0, 0, 0, 60, 0),
+(17502, 200001, 5000000, 773, 150, 0, 0, 0, 60, 0);
