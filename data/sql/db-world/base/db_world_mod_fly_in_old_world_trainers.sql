b78eee7a8196274822e40e12061e4e7be4e41e6e
Stephen Harris <trackmastersteve@users.noreply.github.com>
2026-08-10 20:48:23 -0700

    Update flying trainers SQL for old world
    Replaced trainer_spell insertions with npc_trainer updates for old world flying trainers.

--- data/sql/db-world/base/db_world_mod_fly_in_old_world_trainers.sql
+++ data/sql/db-world/base/db_world_mod_fly_in_old_world_trainers.sql
@@ -1,5 +1,6 @@
 -- Add Custom Old World Flying to Outland & Northrend Flight Trainers
-INSERT IGNORE INTO `trainer_spell` (`TrainerId`, `SpellId`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`) VALUES
-(31238, 200001, 5000000, 773, 225, 60), -- Hira Snowdawn (Dalaran)
-(16654, 200001, 5000000, 773, 225, 60), -- Hargen Bronzewing (Honor Hold)
-(17502, 200001, 5000000, 773, 225, 60); -- Wind Rider Jahubo (Thrallmar)
+DELETE FROM `npc_trainer` WHERE `SpellId` = 200001;
+INSERT INTO `npc_trainer` (`ID`, `SpellId`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`, `ReqSpell`) VALUES
+(31238, 200001, 5000000, 773, 225, 60, 0), -- Hira Snowdawn (Dalaran)
+(16654, 200001, 5000000, 773, 225, 60, 0), -- Hargen Bronzewing (Honor Hold)
+(17502, 200001, 5000000, 773, 225, 60, 0); -- Wind Rider Jahubo (Thrallmar)
