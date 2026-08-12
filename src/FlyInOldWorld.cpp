ea97c392e4946af156053151b17112bc7bdbd3c5
Stephen Harris <trackmastersteve@users.noreply.github.com>
2026-08-12 09:49:20 -0700

    Implement Old World flying functionality and item script
    Added item script for Tome of Old World Flight to allow players to learn the flying spell without client restrictions.

--- src/FlyInOldWorld.cpp
+++ src/FlyInOldWorld.cpp
@@ -4,12 +4,14 @@
 
 #include "ScriptMgr.h"
 #include "Player.h"
+#include "Item.h"
 
 enum {
-    OLD_WORLD_FLYING_SPELL = 200001
+    OLD_WORLD_FLYING_SPELL = 200001,
+    OLD_WORLD_FLYING_ITEM   = 900002
 };
 
-// Add player scripts
+// Add player scripts for zone flight permissions
 class FlyInOldWorld : public PlayerScript
 {
 public:
@@ -24,14 +26,35 @@ public:
             {
                 return false;
             }
-            
         }
         return true;
     }
 };
 
-// Add all scripts in one
+// Item script to handle right-clicking the tome without client spell casting restrictions
+class TomeOfOldWorldFlightScript : public ItemScript
+{
+public:
+    TomeOfOldWorldFlightScript() : ItemScript("TomeOfOldWorldFlightScript") { }
+
+    bool OnUse(Player* player, Item* item, SpellCastTargets const& /*targets*/) override
+    {
+        if (player->HasSpell(OLD_WORLD_FLYING_SPELL))
+        {
+            player->SendSysMessage("You already know Old World Flying.");
+            return false;
+        }
+
+        player->LearnSpell(OLD_WORLD_FLYING_SPELL, false);
+        player->SendSysMessage("You have successfully learned Old World Flying!");
+        player->DestroyItem(item->GetBagSlot(), item->GetSlot(), true);
+        return true;
+    }
+};
+
+// Add all scripts
 void AddFlyInOldWorld()
 {
     new FlyInOldWorld();
+    new TomeOfOldWorldFlightScript();
 }
