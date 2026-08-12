58f25f30b68c373d725c5c5232a262771b945e87
Stephen Harris <trackmastersteve@users.noreply.github.com>
2026-08-12 10:34:50 -0700

    Refactor item and spell mappings for old world flight
    Updated item_template and spell_script_names for Tome of Old World Flight. Adjusted vendor mappings and removed unnecessary fields.

--- data/sql/db-world/base/db_world_mod_fly_in_old_world_trainers.sql
+++ data/sql/db-world/base/db_world_mod_fly_in_old_world_trainers.sql
@@ -4,43 +4,24 @@
 DELETE FROM item_template WHERE entry = 900002;
 
 INSERT INTO item_template (
-    entry, class, subclass, SoundOverrideSubclass, name, displayid, Quality, 
-    Flags, FlagsExtra, BuyCount, BuyPrice, SellPrice, InventoryType, 
-    AllowableClass, AllowableRace, ItemLevel, RequiredLevel, RequiredSkill, 
-    RequiredSkillRank, requiredhonorrank, RequiredCityRank, RequiredReputationFaction, 
-    RequiredReputationRank, maxcount, stackable, ContainerSlots, stat_type1, 
-    stat_value1, stat_type2, stat_value2, stat_type3, stat_value3, stat_type4, 
-    stat_value4, stat_type5, stat_value5, stat_type6, stat_value6, stat_type7, 
-    stat_value7, stat_type8, stat_value8, stat_type9, stat_value9, stat_type10, 
-    stat_value10, ScalingStatDistribution, ScalingStatValue, dmg_min1, dmg_max1, 
-    dmg_type1, dmg_min2, dmg_max2, dmg_type2, armor, holy_res, fire_res, nature_res, 
-    frost_res, shadow_res, arcane_res, delay, ammo_type, RangedModRange, spellid_1, 
-    spelltrigger_1, spellcharges_1, spellppmRate_1, spellcooldown_1, spellcategory_1, 
-    spellcategorycooldown_1, spellid_2, spelltrigger_2, spellcharges_2, spellppmRate_2, 
-    spellcooldown_2, spellcategory_2, spellcategorycooldown_2, spellid_3, spelltrigger_3, 
-    spellcharges_3, spellppmRate_3, spellcooldown_3, spellcategory_3, spellcategorycooldown_3, 
-    spellid_4, spelltrigger_4, spellcharges_4, spellppmRate_4, spellcooldown_4, 
-    spellcategory_4, spellcategorycooldown_4, spellid_5, spelltrigger_5, spellcharges_5, 
-    spellppmRate_5, spellcooldown_5, spellcategory_5, spellcategorycooldown_5, bonding, 
-    description, PageText, LanguageID, PageMaterial, startquest, lockid, Material, 
-    sheath, RandomProperty, RandomSuffix, blockitemset, MaxDurability, area, Map, 
-    BagFamily, TotemCategory, socketColor_1, socketContent_1, socketColor_2, socketContent_2, 
-    socketColor_3, socketContent_3, socketBonus, GemProperties, RequiredDisenchantSkill, 
-    ArmorDamageModifier, duration, ItemLimitCategory, HolidayId, ScriptName, DisenchantID, 
-    FoodType, minMoneyLoot, maxMoneyLoot, flagsCustom, VerifiedBuild
+    entry, class, subclass, name, displayid, Quality, 
+    BuyCount, BuyPrice, SellPrice, InventoryType, 
+    AllowableClass, AllowableRace, ItemLevel, RequiredLevel, 
+    maxcount, stackable, bonding, spellid_1, spelltrigger_1, 
+    spellcharges_1, VerifiedBuild
 ) VALUES (
-    900002, 13, 0, -1, 'Tome of Old World Flight', 61330, 7, 
-    0, 0, 1, 5000000, 100000, 0, -1, -1, 0, 60, 0, 
-    0, 0, 0, 0, 0, 1, 0, 0, 0, 
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
-    0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, NULL, 1000, 0, 0, 0, -- spellid_1 changed to 0
-    0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 
-    0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 
-    0, -1, 0, 0, '', 0, 0, 0, 0, 0, 0, 0, 0, 0, 
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 
-    0, 0, 0, 0, 'TomeOfOldWorldFlightScript', 0, 0, 0, 0, 0, NULL -- ScriptName populated here
+    900002, 13, 0, 'Tome of Old World Flight', 61330, 7, 
+    1, 5000000, 100000, 0, 
+    -1, -1, 0, 60, 
+    1, 0, 0, 200001, 0, 
+    -1, NULL
 );
+
+-- ---------------------------------------------------------------------------
+-- Spell Script Mapping
+-- ---------------------------------------------------------------------------
+DELETE FROM spell_script_names WHERE spell_id = 200001;
+INSERT INTO spell_script_names (spell_id, ScriptName) VALUES (200001, 'spell_tome_of_old_world_flight');
 
 -- ---------------------------------------------------------------------------
 -- Vendor Mappings (Hira Snowdawn, Grunda Bronzewing, etc.)
